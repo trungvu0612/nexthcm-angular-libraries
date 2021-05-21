5eb02d2ca4c74e1b129d9bf845b0fa51b1fcca18
Nguyen Phu Cuong <cuong.nguyen-phu@banvien.com.vn>
2021-05-21 14:26:17 +0700

    HRM-123 fix api req

--- libs/my-time/src/lib/services/leave-type.service.ts
+++ libs/my-time/src/lib/services/leave-type.service.ts
@@ -1,23 +1,26 @@
-import { Inject, Injectable } from '@angular/core';
-import { LeaveTypeState, LeaveTypeStore } from '../state/leave-type/leave-type.store';
-import { PaginatorPlugin } from '@datorama/akita';
-import { HttpClient, HttpParams } from '@angular/common/http';
-import { LEAVE_TYPE_PAGINATOR } from '../state/leave-type/leave-type.paginator';
-import { Observable } from 'rxjs';
-import { LeaveType } from '../models/leave-type';
-import { tap } from 'rxjs/operators';
+import {Inject, Injectable} from '@angular/core';
+import {LeaveTypeState, LeaveTypeStore} from '../state/leave-type/leave-type.store';
+import {PaginatorPlugin} from '@datorama/akita';
+import {HttpClient, HttpParams} from '@angular/common/http';
+import {LEAVE_TYPE_PAGINATOR} from '../state/leave-type/leave-type.paginator';
+import {Observable} from 'rxjs';
+import {LeaveType} from '../models/leave-type';
+import {tap} from 'rxjs/operators';
+import {ENVIRONMENT, Environment} from "@nexthcm/core";
 
 @Injectable({
   providedIn: 'root',
 })
 export class LeaveTypeService {
-  appVersion = '/mytimeapp/v1.0';
+  appVersion = this.env.apiUrl + '/mytimeapp/v1.0';
 
   constructor(
+    @Inject(ENVIRONMENT) protected env: Environment,
     private leaveTypeStore: LeaveTypeStore,
     private httpClient: HttpClient,
     @Inject(LEAVE_TYPE_PAGINATOR) public paginatorRef: PaginatorPlugin<LeaveTypeState>
-  ) {}
+  ) {
+  }
 
   getLeaveTypes(pageIndex: number, pageSize: number): Observable<any> {
     return this.httpClient.get<any>(this.appVersion + '/leave-type', {
@@ -36,7 +39,7 @@ export class LeaveTypeService {
   createLeaveType(dto: LeaveType): Observable<LeaveType> {
     return this.httpClient.post<LeaveType>(this.appVersion + '/leave-type', dto).pipe(
       tap((leaveType) => {
-        this.paginatorRef.clearCache({ clearStore: true });
+        this.paginatorRef.clearCache({clearStore: true});
       })
     );
   }
@@ -44,7 +47,7 @@ export class LeaveTypeService {
   editLeaveType(dto: LeaveType, id: string): Observable<LeaveType> {
     return this.httpClient.put<LeaveType>(this.appVersion + `/leave-type/${id}`, dto).pipe(
       tap((leaveType) => {
-        this.paginatorRef.clearCache({ clearStore: true });
+        this.paginatorRef.clearCache({clearStore: true});
       })
     );
   }
