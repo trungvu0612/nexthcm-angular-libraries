c66c997e8e63609bdf341b84c9e97086e4375ef9
dong.le-bao <dong.le-bao@banvien.com.vn>
2021-08-06 08:38:16 +0700

    feat(admin-contracts): init library

---
+++ libs/admin/contracts/README.md
@@ -0,0 +1,7 @@
+# admin-contracts
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test admin-contracts` to execute the unit tests.
---
+++ libs/admin/contracts/src/lib/admin-contracts.module.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { Route, RouterModule } from '@angular/router';
+
+export const adminContractsRoutes: Route[] = [];
+
+@NgModule({
+  imports: [CommonModule, RouterModule],
+})
+export class AdminContractsModule {}
