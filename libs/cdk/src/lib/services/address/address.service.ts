f41786b47f958d2b0ab685c6cbc19b5167c1235c
dong.le-bao <dong.le-bao@banvien.com.vn>
2021-08-18 00:03:22 +0700

    refactor(address): get places

--- libs/cdk/src/lib/services/address/address.service.ts
+++ libs/cdk/src/lib/services/address/address.service.ts
@@ -1,20 +1,20 @@
 import { HttpClient } from '@angular/common/http';
-import { Address, BaseResponse, PUBLIC_API_PATH } from '@nexthcm/cdk';
 import { Injectable } from '@angular/core';
+import { Address, BaseResponse, PUBLIC_API_PATH } from '@nexthcm/cdk';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
-@Injectable()
+@Injectable({ providedIn: 'root' })
 export class AddressService {
   constructor(private http: HttpClient) {}
 
-  getPlaces(parendId?: string): Observable<Address[]> {
-    return this.http.get<Address[]>(`${PUBLIC_API_PATH}/address`, { params: { uuid: parendId || '' } });
+  getPlaces(parentId?: string): Observable<Address[]> {
+    return this.http.get<Address[]>(`${PUBLIC_API_PATH}/address`, { params: { uuid: parentId || '' } });
   }
 
   getAddress(id: string): Observable<Address> {
     return this.http
-      .get<BaseResponse<Address>>('/publicapp/v1.0/address/' + id)
+      .get<BaseResponse<Address>>(`${PUBLIC_API_PATH}/address/${id}`)
       .pipe(map((response) => response.data));
   }
 }
