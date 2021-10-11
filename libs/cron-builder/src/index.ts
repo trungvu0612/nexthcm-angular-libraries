71a94f82bb9a2d7b9524e3c1d83ebe7e34be8670
baodongle <baodongle@gmail.com>
2021-10-11 17:21:28 +0700

    fix(cron-builder): export quartz-cron

--- libs/cron-builder/src/index.ts
+++ libs/cron-builder/src/index.ts
@@ -1,4 +1,5 @@
 export * from './lib/cron-builder.module';
+export * from './lib/quartz-cron/quartz-cron.component';
 export * from './lib/tokens';
 export * from './lib/models';
 export * from './lib/constants/localizations';
