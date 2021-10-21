af6df8c17a406185eede560863b75e3f561bc3b2
baodongle <baodongle@gmail.com>
2021-10-21 12:49:34 +0700

    chore(all): migrate nx to v13

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,7 @@ process.env.TAILWIND_MODE = guessProductionMode() ? 'build' : 'watch';
 
 module.exports = {
   prefix: '',
-  mode: 'jit',
-  purge: {
-    content: ['./apps/**/*.{html,ts,css,scss,sass,less,styl}', './libs/**/*.{html,ts,css,scss,sass,less,styl}'],
-  },
-  darkMode: false, // or 'media' or 'class'
+  content: ['./apps/**/*.{html,ts,css,scss,sass,less,styl}', './libs/**/*.{html,ts,css,scss,sass,less,styl}'],
   theme: {
     extend: {},
   },
