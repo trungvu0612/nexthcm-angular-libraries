8cabb8bd37531586752d82fde8dd64f704679c8f
baodongle <baodongle@gmail.com>
2021-10-25 00:06:48 +0700

    chore(all): update packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ process.env.TAILWIND_MODE = guessProductionMode() ? 'build' : 'watch';
 module.exports = {
   prefix: '',
   content: ['./apps/**/*.{html,ts,css,scss,sass,less,styl}', './libs/**/*.{html,ts,css,scss,sass,less,styl}'],
+  darkMode: 'class',
   theme: {
     extend: {},
   },
