dba21718d463047eb4667a5efd3953dcd9416bae
dong.le-bao <dong.le-bao@banvien.com.vn>
2022-03-10 16:33:19 +0700

    build: revert environment files

--- apps/demo-web/src/environments/environment.prod.ts
+++ apps/demo-web/src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
-export const environment = {
+import { AppConfig } from '@nexthcm/core';
+
+export const environment: AppConfig = {
   production: true,
+  language: 'en',
+  baseUrl: 'https://hcm.banvien.com.vn',
+  apiUrl: 'https://api-hcm.banvien.com.vn',
+  orgUrl: 'https://api-hcm.banvien.com.vn',
 };
---
+++ apps/demo-web/src/environments/environment.qa.ts
@@ -0,0 +1,9 @@
+import { AppConfig } from '@nexthcm/core';
+
+export const environment: AppConfig = {
+  production: true,
+  language: 'en',
+  baseUrl: 'https://qa-nexthcm.banvien.com.vn',
+  apiUrl: 'https://qa-nexthcm-api.banvien.com.vn',
+  orgUrl: 'https://qa-nexthcm-api.banvien.com.vn',
+};
