61f5333fb8104a2364ae2b73b736619a848f64d6
phuc.le-hong2 <phuc.le-hong2@banvien.com.vn>
2022-04-27 17:11:02 +0700

    refactor(ui): layout

--- libs/ui/src/lib/modules/layout/layout.module.ts
+++ libs/ui/src/lib/modules/layout/layout.module.ts
@@ -1,19 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { GetFilePipeModule, SkipNavigationDirectiveModule } from '@nexthcm/cdk';
-import { SvgIconsModule } from '@ngneat/svg-icon';
 import { TranslocoModule } from '@ngneat/transloco';
 import { LetModule } from '@rx-angular/template';
 import {
   TuiButtonModule,
   TuiDataListModule,
-  TuiDropdownModule,
   TuiHostedDropdownModule,
   TuiScrollbarModule,
   TuiSvgModule,
 } from '@taiga-ui/core';
-import { TuiAvatarModule, TuiTabsModule } from '@taiga-ui/kit';
+import { TuiTabsModule } from '@taiga-ui/kit';
 import { NgxPermissionsModule } from 'ngx-permissions';
 
 import { AvatarComponentModule } from '../../components';
@@ -26,21 +23,16 @@ import { PageComponent } from './page/page.component';
   imports: [
     CommonModule,
     RouterModule,
-    TuiDataListModule,
-    TuiDropdownModule,
-    TuiTabsModule,
-    TuiAvatarModule,
-    TuiSvgModule,
+    AvatarComponentModule,
     TranslocoModule,
-    TuiHostedDropdownModule,
-    TuiButtonModule,
-    SvgIconsModule,
-    TuiScrollbarModule,
-    SkipNavigationDirectiveModule,
     NgxPermissionsModule,
     LetModule,
-    GetFilePipeModule,
-    AvatarComponentModule,
+    TuiTabsModule,
+    TuiDataListModule,
+    TuiSvgModule,
+    TuiHostedDropdownModule,
+    TuiButtonModule,
+    TuiScrollbarModule,
   ],
   exports: [LayoutComponent, PageComponent],
 })
