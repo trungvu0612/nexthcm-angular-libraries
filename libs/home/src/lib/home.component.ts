4c59ef93dde7156a38f655dcc1becb537dbe4f34
phuc.le-hong2 <phuc.le-hong2@banvien.com.vn>
2022-05-25 09:26:54 +0700

    feat(home): working time

--- libs/home/src/lib/home.component.ts
+++ libs/home/src/lib/home.component.ts
@@ -2,13 +2,13 @@ import { HttpParams } from '@angular/common/http';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { AuthService } from '@nexthcm/auth';
 import { PromptService, UserProfileService } from '@nexthcm/cdk';
-import { MyTimeService, WorkingHours } from '@nexthcm/my-time';
+import { MyTimeService } from '@nexthcm/my-time';
 import { GeolocationService } from '@ng-web-apis/geolocation';
 import { TranslocoService } from '@ngneat/transloco';
 import { isPresent, tuiPure } from '@taiga-ui/cdk';
 import { endOfToday, startOfYesterday } from 'date-fns';
 import { BehaviorSubject, from, Observable, of, Subject } from 'rxjs';
-import { catchError, filter, map, share, startWith, switchMap, take, tap } from 'rxjs/operators';
+import { catchError, filter, map, startWith, switchMap, take, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'hcm-home',
@@ -17,22 +17,19 @@ import { catchError, filter, map, share, startWith, switchMap, take, tap } from 
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomeComponent {
-  readonly queryParams$ = new BehaviorSubject(
-    new HttpParams()
-      .set('userId', this.authService.get('userInfo', 'userId'))
-      .set('fromDate', startOfYesterday().getTime())
-      .set('toDate', endOfToday().getTime())
-  );
   readonly fullName$ = this.userProfileService.select('general', 'fullName');
-  readonly monthWorkingTime$ = this.myTimeService.getWorkingTimeCurrentMonth();
+  readonly monthWorkingTime$ = this.myTimeService.getWorkingTimeCurrentMonth().pipe(startWith(null));
+
+  readonly fetch$ = new Subject<void>();
+  readonly checkInOut$ = new Subject<void>();
   readonly shouldCheckedOut$ = new BehaviorSubject<boolean | null>(false);
+
   readonly checkedId$ = this.myTimeService.getTimekeepingLog().pipe(
     tap((item) => this.shouldCheckedOut$.next(item.id ? !!item.inTime : null)),
     map((item) => item.id),
     filter(isPresent)
   );
-  readonly checkInOut$ = new Subject<void>();
-  private readonly checkInOutHandler$ = this.checkInOut$.pipe(
+  readonly checkInOutLoading$ = this.checkInOut$.pipe(
     switchMap(() =>
       this.geolocation$.pipe(
         take(1),
@@ -52,40 +49,41 @@ export class HomeComponent {
                 ? this.myTimeService.checkInOut({ typeCheckInOut: 'web-app', latitude, longitude }).pipe(
                     tap(
                       this.promptService.handleResponse(
-                        this.shouldCheckedOut$.value ? 'checkOutSuccessfully' : 'checkInSuccessfully',
-                        () => {
-                          this.shouldCheckedOut$.next(true);
-                          this.queryParams$.next(this.queryParams$.value);
-                        }
+                        this.shouldCheckedOut$.value ? 'checkOutSuccessfully' : 'checkInSuccessfully'
                       )
-                    )
+                    ),
+                    tap(() => {
+                      this.shouldCheckedOut$.next(true);
+                      this.fetch$.next();
+                    }),
+                    startWith(null)
                   )
                 : of(true)
             ),
             catchError(() => of(true))
           )
         ),
-        catchError((err: GeolocationPositionError) => this.showGeolocationError(err)),
-        startWith(null)
+        catchError((err: GeolocationPositionError) => this.showGeolocationError(err))
       )
-    )
-  );
-  readonly checkInOutLoading$ = this.checkInOutHandler$.pipe(
+    ),
     startWith('init'),
     switchMap((value) => (value === 'init' ? this.checkedId$.pipe(startWith(null)) : of(value))),
     map((value) => !value)
   );
-  private readonly myWorkingDaysRequest$ = this.queryParams$.pipe(
-    switchMap((params) =>
-      this.myTimeService.getWorkingHoursOfOnlyMe(params).pipe(
+
+  private readonly params = new HttpParams()
+    .set('userId', this.authService.get('userInfo', 'userId'))
+    .set('fromDate', startOfYesterday().getTime())
+    .set('toDate', endOfToday().getTime());
+  readonly myWorkingDays$ = this.fetch$.pipe(
+    startWith(null),
+    switchMap(() =>
+      this.myTimeService.getWorkingHoursOfOnlyMe(this.params).pipe(
         map((res) => res.items),
         startWith(null)
       )
-    ),
-    share()
+    )
   );
-  readonly loading$ = this.myWorkingDaysRequest$.pipe(map((value) => !value));
-  readonly myWorkingDays$: Observable<WorkingHours[]> = this.myWorkingDaysRequest$.pipe(filter(isPresent));
 
   constructor(
     private readonly myTimeService: MyTimeService,
