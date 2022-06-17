c8321ff50720b6e76ccdca90fbf2ba3f8d82a247
tranquangvan <van.tran-quang@banvien.com.vn>
2022-06-17 15:22:34 +0700

    call server publickey

--- apps/demo-web/src/environments/environment.prod.ts
+++ apps/demo-web/src/environments/environment.prod.ts
@@ -5,5 +5,4 @@ export const environment: AppConfig = {
   language: 'en',
   baseUrl: 'https://hcm.banvien.com.vn',
   apiUrl: 'https://api-hcm.banvien.com.vn',
-  serverPublicKey: 'BLqtD0jrZJ5jAEYCrp1I7sLVqZeT77sLn5_Aptn159jyfEvsv-ZR6L9tMJJp3sew0G2NiE8UYdGE9bKEoXyEERE',
 };
--- apps/demo-web/src/environments/environment.qa.ts
+++ apps/demo-web/src/environments/environment.qa.ts
@@ -5,5 +5,4 @@ export const environment: AppConfig = {
   language: 'en',
   baseUrl: 'https://qa-nexthcm.banvien.com.vn',
   apiUrl: 'https://qa-nexthcm-api.banvien.com.vn',
-  serverPublicKey: 'BLqtD0jrZJ5jAEYCrp1I7sLVqZeT77sLn5_Aptn159jyfEvsv-ZR6L9tMJJp3sew0G2NiE8UYdGE9bKEoXyEERE',
 };
